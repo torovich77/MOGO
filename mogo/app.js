929a06f593e1bc5cfc6a9ba153f4d2d761561b79
torovich77 <60387138+torovich77@users.noreply.github.com>
2021-12-09 13:06:16 +0300

    Add files via upload

---
+++ mogo/app.js
@@ -0,0 +1,8 @@
+let accord = document.querySelector('.services2__slider--subtitle');
+let button = document.querySelector('.arow');
+    
+
+
+button.onclick = function () {
+        accord.classList.remove('hide');
+    };
